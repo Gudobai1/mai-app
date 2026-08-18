6e2e7f2f85481327113efb82923ad0ad3d8be8ec
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-18 16:51:29 -0300

    Integrar detalhes finais de paridade dos módulos

--- app/v2/V2Areas.tsx
+++ app/v2/V2Areas.tsx
@@ -5,6 +5,7 @@ import { AreaView as BaseAreaView } from './V2AreasBase'
 import type { SecondaryView } from './V2AreasBase'
 import { LegacyParityExtras } from './LegacyParityExtras'
 import { HealthFullParity } from './HealthFullParity'
+import { ModuleDetailParity } from './ModuleDetailParity'
 
 export type { SecondaryView } from './V2AreasBase'
 
@@ -21,6 +22,7 @@ export function AreaView(props: Props) {
   return <div style={{ position: 'relative', minHeight: '100%' }}>
     <BaseAreaView {...props} />
     <LegacyParityExtras {...props} />
+    <ModuleDetailParity {...props} />
     {props.view === 'health' && <HealthFullParity state={props.state} today={props.today} commit={props.commit} />}
   </div>
 }
