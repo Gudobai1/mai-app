4cb4f27610425d77f2d7a2497e9d745be16b9f77
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-19 17:29:49 -0300

    Ativar reconstrução completa do MAI

--- app/v2/MaiV2ParityShell.tsx
+++ app/v2/MaiV2ParityShell.tsx
@@ -1,16 +1,7 @@
 'use client'
 
-import { useState } from 'react'
-import { MaiV2App } from './MaiV2App'
-import { TaskProjectParity } from './TaskProjectParity'
+import { UnifiedApp } from './UnifiedApp'
 
 export function MaiV2ParityShell() {
-  const [revision, setRevision] = useState(0)
-  return <>
-    <MaiV2App key={revision} />
-    <TaskProjectParity onChanged={() => setRevision(value => value + 1)} />
-    <style jsx global>{`
-      button[style*="z-index: 8050"] { bottom: 132px !important; }
-    `}</style>
-  </>
+  return <UnifiedApp />
 }
