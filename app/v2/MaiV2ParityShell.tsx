433e4fdae00ae99f023e60c3a8be08e89ab66dfb
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-19 18:04:41 -0300

    Aplicar camada global de UX

--- app/v2/MaiV2ParityShell.tsx
+++ app/v2/MaiV2ParityShell.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 import { UnifiedApp } from './UnifiedApp'
+import { UxController } from './UxController'
 
 export function MaiV2ParityShell() {
-  return <UnifiedApp />
+  return <>
+    <UnifiedApp />
+    <UxController />
+  </>
 }
