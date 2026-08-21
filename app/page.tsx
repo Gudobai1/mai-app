3ef658009401b51823381de5697556ac0850ccc0
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-21 08:33:57 -0300

    Bloquear MAI sem sessão Google autorizada

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { GOOGLE_COOKIE, hasAllowedGoogleCookie } from '../lib/google'
 import { MaiV2ParityShell } from './v2/MaiV2ParityShell'
 
-export default function Page() {
+export default async function Page() {
+  const store = await cookies()
+  if (!hasAllowedGoogleCookie(store.get(GOOGLE_COOKIE)?.value)) redirect('/login')
   return <MaiV2ParityShell />
 }
