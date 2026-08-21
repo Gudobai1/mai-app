38bba9f6e47754bb93c63c1bd2156b7422f9202c
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-21 19:48:20 -0300

    Ativar decoração global das datas

--- app/PwaRegister.tsx
+++ app/PwaRegister.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { useEffect } from 'react'
+import { ItemDateTone } from './v2/ItemDateTone'
 
 export function PwaRegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js').catch(() => null)
   }, [])
-  return null
+  return <ItemDateTone />
 }
