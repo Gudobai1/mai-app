cccaa11d2dc34a0ad9484325a12eacfa9615d7d7
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-24 13:41:12 -0300

    Ativar estilos da visualização Kanban

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ import './mai-upcoming-v2.css'
 import './mai-upcoming-v3.css'
 import './mai-mobile-safe-zone.css'
 import './mai-overlay-layer-fix.css'
+import './mai-kanban.css'
 import './mai-dark-mode-v2.css'
 
 export const metadata: Metadata = {
