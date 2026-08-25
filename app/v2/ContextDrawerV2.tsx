44d06fe9233a56e46a085a160839fd9c38d0fccc
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-25 14:22:33 -0300

    Ativar anexos funcionais nos editores de itens

--- app/v2/ContextDrawerV2.tsx
+++ app/v2/ContextDrawerV2.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import type { ComponentProps } from 'react'
+import { AttachmentPortal } from './AttachmentPortal'
 import { ContextDrawerV2 as GeneralContextDrawer } from './ContextDrawerV3'
 import { GeneralContextDrawerV4 } from './GeneralContextDrawerV4'
 import { TaskContextDrawerV4 } from './TaskContextDrawerV4'
@@ -8,6 +9,7 @@ import { TaskContextDrawerV4 } from './TaskContextDrawerV4'
 type Props = ComponentProps<typeof GeneralContextDrawer>
 
 export function ContextDrawerV2(props: Props) {
-  if (props.item?.kind === 'task') return <TaskContextDrawerV4 {...props} />
-  return <GeneralContextDrawerV4 {...props} />
+  if (!props.item) return null
+  const drawer = props.item.kind === 'task' ? <TaskContextDrawerV4 {...props} /> : <GeneralContextDrawerV4 {...props} />
+  return <>{drawer}<AttachmentPortal item={props.item} state={props.state} commit={props.commit} googleRpc={props.googleRpc} /></>
 }
