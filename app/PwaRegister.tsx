b174d8784398ab4f6e95c9d28e509bcf779d3cb7
Gudobai1 <101523841+Gudobai1@users.noreply.github.com>
2026-08-28 10:44:08 -0300

    Atualizar service worker sem cache intermediário

--- app/PwaRegister.tsx
+++ app/PwaRegister.tsx
@@ -7,7 +7,8 @@ import { ModuleListSorter } from './v2/ModuleListSorter'
 
 export function PwaRegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js').catch(() => null)
+    if (!('serviceWorker' in navigator)) return
+    void navigator.serviceWorker.register('/sw.js', { updateViaCache: 'none' }).then(registration => registration.update()).catch(() => null)
   }, [])
   return <><ItemDateTone/><ModuleListSorter/><GlobalFilePreviewBridge/></>
 }
